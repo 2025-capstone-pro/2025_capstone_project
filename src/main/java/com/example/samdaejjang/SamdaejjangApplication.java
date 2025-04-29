ea9588d5e84712c44b170897b78eb4d423cb0fd9
kae-mi <rudahyang7815@gmail.com>
2025-04-29 20:45:56 +0900

    initial commit

---
+++ src/main/java/com/example/samdaejjang/SamdaejjangApplication.java
@@ -0,0 +1,13 @@
+package com.example.samdaejjang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SamdaejjangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SamdaejjangApplication.class, args);
+	}
+
+}
